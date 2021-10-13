44866503b91beeba1b867cdb775522b7d4be1d9d
emonmahdi <developeremon1997@gmail.com>
2021-10-13 09:37:19 +0600

    my commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
+import initialAuthentication from './components/Firebase/firebase.init';
 import Header from './components/Header/Header';
 import Inventory from './components/Inventory/Inventory';
 import NotFound from './components/NotFound/NotFound';
 import OrderReview from './components/OrderReview/OrderReview';
 import PlaceOrder from './components/PlaceOrder/PlaceOrder';
 import Shop from './components/Shop/Shop';
+
+initialAuthentication();
 
 function App() {
   return (
---
+++ src/components/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCXE8nOo4OZ_b7sj5JvnKCypWY3OYgRJXg",
+    authDomain: "ema-john-simple-2a444.firebaseapp.com",
+    projectId: "ema-john-simple-2a444",
+    storageBucket: "ema-john-simple-2a444.appspot.com",
+    messagingSenderId: "748019845073",
+    appId: "1:748019845073:web:bb5ff1eada2c23443082f0"
+};
+
+export default firebaseConfig;
